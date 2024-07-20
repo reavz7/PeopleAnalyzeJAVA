16adb2183b1ea702f7a0413663df1791eaadd1eb
Kamil Stępień <130293368+reavz7@users.noreply.github.com>
2024-07-20 11:48:53 +0200

    v. 0.1
    Added implementations to file manipulation functions

--- LOGICjava/src/main/java/humanproject/ImplFileOperator.java
+++ LOGICjava/src/main/java/humanproject/ImplFileOperator.java
@@ -1,15 +1,46 @@
 package humanproject;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
-public class ImplFileOperator implements  IFileOperator{
+public class ImplFileOperator implements  IFileOperator {
     @Override
-    public void serialize(List<Human> users, String filename) {
-
+    public void serialize(List<Human> humans, String filename) {
+        String csvFile = "src/main/resources/" + filename + ".csv";
+        try (FileWriter writer = new FileWriter(csvFile)) {
+            for (Human human : humans) {
+                writer.write(human.getName() + "," + human.getSurname() + "," + human.getAge() + "," + human.getEmail() + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Zapisano do pliku " + filename);
     }
 
     @Override
     public List<Human> deserialize(String filename) {
-        return List.of();
+        List<Human> humans = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/" + filename));
+             CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT)) {
+            for (CSVRecord csvRecord : csvParser) {
+                String name = csvRecord.get(0);
+                String surname = csvRecord.get(1);
+                Integer age = Integer.parseInt(csvRecord.get(2));
+                String email = csvRecord.get(3);
+                Human human = new Human(name, surname, age, email);
+                humans.add(human);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return humans;
     }
 }
